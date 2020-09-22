7d9f057603040a2040c97d0b21c24257e6e15783
LdMe <dlafuente003@gmail.com>
2020-09-22 16:29:29 +0200

    comments added to installer script

--- microsoft-teams-install.sh
+++ microsoft-teams-install.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-
+## update and install curl to download the installer
 sudo apt update
 sudo apt install curl
 
+## download the teams app installer
 curl https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.16851_amd64.deb --output teams.deb
 
+## install teams for linux
 sudo dpkg -i teams.deb
 
+## remove the installer
 rm teams.deb
 
 echo "Microsoft Teams succesfully installed"
