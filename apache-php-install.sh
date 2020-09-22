b6f45a5159f6fd8ca4789e62116dea6f7e3d1853
LdMe <dlafuente003@gmail.com>
2020-09-22 15:42:03 +0200

    server root dir path can be easily changed

--- apache-php-install.sh
+++ apache-php-install.sh
@@ -1,46 +1,48 @@
 #! /bin/bash
 
-#install and configure mysql
+## directory where we are going to save all the Coding Academy projects
+RenduDir="$HOME/Rendu"
+
+## install and configure mysql
 sudo apt install mysql-server
 
 #
-# uncomment next line to configure mysql password
+## uncomment next line to configure mysql password and settings
 #
 #sudo mysql_secure_installation
 
 
-# install some compilers that may be neccessary
+## install some compilers that may be neccessary
 sudo apt install build-essential
-#install apache2 and php and configure localhost 
+
+## install apache2 and php and configure localhost 
 sudo apt install apache2
 sudo apt install php libapache2-mod-php
 sudo systemctl restart apache2
-# copy apache config files to your apache directory
+
+## copy apache config files to your apache directory
 sudo cp 000-coding.conf /etc/apache2/sites-available/
 sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf_old
 sudo cp apache2.conf /etc/apache2/
 
-# change <home_dir> for your home directory path in apache configuration files
-sudo sed -i "s@<home_dir>@$HOME@g" /etc/apache2/sites-available/000-coding.conf
-sudo sed -i "s@<home_dir>@$HOME@g" /etc/apache2/apache2.conf
+## change <home_dir> for your home directory path in apache configuration files
+sudo sed -i "s@<home_dir>@$RenduDir@g" /etc/apache2/sites-available/000-coding.conf
+sudo sed -i "s@<home_dir>@$RenduDir@g" /etc/apache2/apache2.conf
 
-# enable anew apache configuration
+## enable new apache configuration
 sudo a2ensite 000-coding.conf
 sudo systemctl restart apache2
 sudo systemctl reload apache2
 
-# create your Rendu directory and add an index.php file to check it works
-mkdir -p ~/Rendu
-chmod 755 ~/Rendu
+## create your Rendu directory and add execution permissions
+mkdir -p $RenduDir
+chmod 755 $RenduDir
 
-#greetings="hello $USER"; 
-#echo "<?php echo \"<h1>$greetings</h1> \"; " > ~/Rendu/index.php
-#echo "echo \"<p>You can edit this file in $HOME/Rendu/index.php</p>\";?>" >> ~/Rendu/index.php
+## copy example index file and replace generic username for your own username
+cp index.php $RenduDir/index.php
+sudo sed -i "s@<username>@$USER@g" $RenduDir/index.php
 
-cp index.php ~/Rendu/index.php
-sudo sed -i "s@username@$USER@g" ~/Rendu/index.php
-
-sudo sed -i "s@<home>@$HOME@g" ~/Rendu/index.php
+sudo sed -i "s@<home>@$RenduDir@g" $RenduDir/index.php
 
 echo -e "\n\nInstallation complete. Everything should be ready if there was no error"
 echo -e "\n\nYou can check if the server is working by going to 'localhost:80' in your browser\n"
--- index.php
+++ index.php
@@ -2,49 +2,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome username</title>
+	<title>Welcome <username></title>
+	<!--
+		---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		links to enable bootstrap in the page
+		---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	<!--
+	-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	-->
+	<!--
+		apply some CSS style directly to the page elements
+	-->
 	<style type="text/css">
 		body {
 			color: white;
+			height:100vh;
+			background-size: cover;
+			background-attachment: fixed;
+			background-repeat: no-repeat ;
+			background-image: url('https://media.istockphoto.com/photos/blue-abstract-background-or-texture-picture-id1138395421?k=6&m=1138395421&s=612x612&w=0&h=bJ1SRWujCgg3QWzkGPgaRiArNYohPl7-Wc4p_Fa_cyA=');
 		}
 		h1 {
 			margin-top: 10px;
 			text-align: center;
 		}
+		nav {
+			border-radius: 10px;
+			background-color: rgba(150,150,255,0.3);
+		}
+		.nav-img {
+			max-height: 50px;	
+		}
+		.element {
+			border-radius: 10px;
+			background-color: rgba(200,200,255,0.2);
+		}
 	</style>
 </head>
-<body style="height:100vh;background-size: cover; background-attachment: fixed;background-repeat: no-repeat ;background-image: url('https://media.istockphoto.com/photos/blue-abstract-background-or-texture-picture-id1138395421?k=6&m=1138395421&s=612x612&w=0&h=bJ1SRWujCgg3QWzkGPgaRiArNYohPl7-Wc4p_Fa_cyA=');">
+<body>
 	
 	<div class="container-fluid" >
 
 
-		<nav class="navbar navbar-light " style="border-radius: 10px;background-color: rgba(150,150,255,0.3)">
+		<nav class="navbar navbar-light " >
 			<div class="row">
 				<div class=col-6>
-					<img src="https://newsroom.ionis-group.com/wp-content/uploads/2018/12/epitech-logo-quadri-baseline-anglais.png" alt="Coding Academy Logo"style="max-height: 50px">
+					<img  class="nav-img" src="https://newsroom.ionis-group.com/wp-content/uploads/2018/12/epitech-logo-quadri-baseline-anglais.png" alt="Coding Academy Logo">
 				</div>
 				<div class=col-6>
-					<img src="https://newsroom.ionis-group.com/wp-content/uploads/2019/09/CODING_ACADEMY_LOGO_CMJN.png" alt="Coding Academy Logo" style="max-height: 50px">
+					<img class="nav-img" src="https://newsroom.ionis-group.com/wp-content/uploads/2019/09/CODING_ACADEMY_LOGO_CMJN.png" alt="Coding Academy Logo" >
 				</div>
 			</div>
 			<a class="nav-link" href="https://intra.epitech.eu/">Go to the intranet</a>
 		</nav>
 		<?php
-		echo "<h1>Hello username</h1> "; 
+		echo "<h1>Hello <username></h1> "; 
 		?>
-		<div class="container-fluid p-2 mt-2" style="background-color: rgba(200,200,255,0.2)" >
+		<div class="container-fluid p-2 mt-2 element" >
 			<?php 
 
-			echo "This is a test page, if you see it, it means your apache server and php are working in your computer.";
-			echo "<p>You can edit this file in <b><home>/Rendu/index.php</b></p>";
+			echo "This is a test page, if you see it, it means your Apache http server and PHP are working in your computer.";
+			echo "<p>You can edit this file in <b><home>/index.php</b></p>";
 			?>
 		</div>
 		
-		<div class="container-fluid p-2 mt-2" style="background-color: rgba(200,200,255,0.2)" >
+		<div class="container-fluid p-2 mt-2 element"  >
 			<p>This installer was downloaded with Git from Github. For more info, check out these links</p>
 			<ul>
 				<li>
@@ -55,7 +82,7 @@
 				</li>
 			</ul>
 		</div>
-		<div class="container-fluid p-2 mt-2" style="background-color: rgba(200,200,255,0.2)" >
+		<div class="container-fluid p-2 mt-2 element"  >
 		<p>If you want to learn how to use your computer from a terminal, you can read about bash in the next links</p>
 		<ul>
 			<li>
@@ -66,7 +93,7 @@
 			</li>
 		</ul>
 		</div>
-		<div class="container-fluid p-2 mt-2" style="background-color: rgba(200,200,255,0.2)" >
+		<div class="container-fluid p-2 mt-2 element"  >
 		<p>Technology behind this page:</p>
 		<ul>
 			<li><a href="https://httpd.apache.org/">Apache http server</a></li>
